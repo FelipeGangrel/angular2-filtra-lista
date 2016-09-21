100720b1785efae0224b7e529b54db25455f7fd8
Felipe Gangrel <felipefrog@gmail.com>
2016-09-20 22:02:10 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Lançamentos';
+
+  public games: Array<any>;
+  public gamesMostrar: Array<any>;
+  public plataformaSelecionada; 
+
+  constructor(){
+
+    this.games = [
+      { nome: "Bloodborne", plataformas: ['PS4'] },
+      { nome: "Battlefield 1", plataformas: ['PS4','XBOX','PC']},
+      { nome: "Pokemon Sun", plataformas: ['3DS']},
+      { nome: "Pokemon Moon", plataformas: ['3DS']},
+      { nome: "The Last Guardian", plataformas: ['PS4']},
+      { nome: "Forza Horizon 3", plataformas: ['XBOX','PC']},
+      { nome: "Super Mario Maker", plataformas: ['WIIU']},
+      { nome: "Zelda Breath of the Wild", plataformas: ['WIIU', 'NX']}
+    ];
+
+    this.gamesMostrar = this.games;
+  }
+
+  mudaPlataforma(event: any, plataforma: string){
+    let pl = plataforma.toUpperCase();
+    console.log(pl);
+    if(plataforma){
+      this.gamesMostrar = this.games
+        .filter( game => (game.plataformas.indexOf(pl) !== -1) );
+    }else{
+      this.gamesMostrar = this.games;
+    }
+  }
+
+
 }
